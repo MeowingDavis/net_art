2a3a1a82a66681f9421bfe95a6bbc4746c42b364
Davis Lang <s3775656@student.rmit.edu.au>
2023-04-28 19:07:23 +1000

    update

--- public/sketch.js
+++ public/sketch.js
@@ -1,50 +1,48 @@
-document.body.style.margin   = 0
-document.body.style.overflow = `hidden`
+document.body.style.margin = 0;
+document.body.style.overflow = `hidden`;
 
-let r
+let r;
 
 function setup() {
   createCanvas(innerWidth, innerHeight);
-  colorMode (HSB)
-  r = new RecursiveRectangle (0, 0, width, height, rand_colour ())
-  noStroke ();
+  colorMode(HSB);
+  r = new RecursiveCircle(width/2, height/2, min(width, height), rand_colour());
+  noStroke();
 }
 
 function draw() {
   background(220);
-  r.draw (frameCount)
+  r.draw(frameCount);
 }
 
-class RecursiveRectangle {
-  constructor (x, y, w, h, c) {
-    this.x = x
-    this.y = y
-    this.w = w
-    this.h = h
-    this.c = c
-    this.has_child = false
-    this.n_x = random () * 20
-    this.n_y = random () * 20
-    
-    if (w > 0 && h > 0) {
-      const c = rand_colour ()
-      this.child = new RecursiveRectangle (x + 12, y + 12, w - 24, h - 24, c)
-      this.has_child = true
+class RecursiveCircle {
+  constructor(x, y, d, c) {
+    this.x = x;
+    this.y = y;
+    this.d = d;
+    this.c = c;
+    this.has_child = false;
+    this.n_x = random() * 20;
+    this.n_y = random() * 20;
+
+    if (d > 10) {
+      const c = rand_colour();
+      this.child = new RecursiveCircle(x, y, d * 0.75, c);
+      this.has_child = true;
     }
   }
-  
-  draw (f) {
-    fill (this.c)
-    const x_offset = noise (this.n_x + (f / 60), this.n_y) * 12 - 6
-    const y_offset = noise (this.n_x, this.n_y + (f / 60)) * 12 - 6
-    rect (this.x + x_offset, this.y + y_offset, this.w, this.h)
+
+  draw(f) {
+    fill(this.c);
+    const offset = noise(this.n_x + (f / 60), this.n_y) * 60 - 30;
+    ellipse(this.x, this.y, this.d + offset, this.d + offset);
     if (this.has_child) {
-      this.child.draw (f)
+      this.child.draw(f);
     }
-  }  
+  }
 }
 
-function rand_colour () {
-  const h = random (360)
-  return color (h, 100, 100)
+function rand_colour() {
+  const h = random(360);
+  return color(h, 100, 100);
 }
